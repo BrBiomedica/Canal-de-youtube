0c60f5d7083b47dbe2d4fd96cd4043b4fc8e9b4a
Br Biomedica <83718545+BrBiomedica@users.noreply.github.com>
2021-05-28 11:21:13 -0500

    Add files via upload

---
+++ pantalla_oled.ino
@@ -0,0 +1,38 @@
+#define __DEBUG__
+
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+void setup() {
+#ifdef __DEBUG__
+  Serial.begin(9600);
+#endif
+
+  // Iniciar pantalla OLED en la dirección 0x3C
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+#ifdef __DEBUG__
+    Serial.println("No se encuentra la pantalla OLED");
+#endif
+    while (true);
+  }
+
+  // Limpiar buffer
+  display.clearDisplay();
+  // Tamaño del texto
+  display.setTextSize(1);
+  // Color del texto
+  display.setTextColor(SSD1306_WHITE);
+  // Posición del texto
+  display.setCursor(10, 32);
+  // Escribir texto
+  display.println("Suscribete");
+  // Enviar a pantalla
+  display.display();
+
+}
+
+void loop() {}
